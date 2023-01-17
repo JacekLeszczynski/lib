ffae75d48c54ca80a7297d062adcb5270221aa58
Jacek Leszczynski <sam@bialan.pl>
2023-01-17 09:46:30 +0100

    Wersja aktualna

--- ecode.pas
+++ ecode.pas
@@ -30,10 +30,46 @@ Uwagi:
   01: Co do stringów, wszystkie te funkcje używają typu string.
 }
 
+{Przykład użycia funkcji QSort:
+
+function CompareF(const A, B: Pointer): Integer;
+begin
+  Result := PInteger(A)^ - PInteger(B)^;
+end;
+
+procedure TForm1.BitBtn4Click(Sender: TObject);
+var
+  t: array [1..100] of integer;
+  i,a: integer;
+  s: string;
+  compar: TComparisonFunc;
+begin
+  i:=1;
+  a:=0;
+  while true do
+  begin
+    s:=GetLineToStr(Edit13.Text,i,',');
+    if s='' then break;
+    inc(a);
+    t[a]:=StrToInt(s);
+    inc(i);
+  end;
+  compar:=@CompareF;
+  qsort(@t,a,sizeof(integer),compar);   // <<<< TUTAJ!
+  s:='';
+  for i:=1 to a do s:=s+','+IntToStr(t[i]);
+  if s[1]=',' then delete(s,1,1);
+  Edit15.Text:=s;
+end;
+}
+
 interface
 
 uses
   Classes, Types;
+
+type
+  TComparisonFunc = function(const A, B: Pointer): Integer;
 
 var
   _FF: string[1];
@@ -69,16 +105,18 @@ function CreateString(c:char;l:integer):string; cdecl; external 'libecode.so' na
 function EncryptString(s,token: string;force_length:integer=0): string; cdecl; external 'libecode.so' name 'EncryptString';
 function DecryptString(s,token: string; trim_spaces: boolean = false): string; cdecl; external 'libecode.so' name 'DecryptString';
 { ----------------------- KOD CZASU ----------------------------- }
-function SecToTime(aSec: longword): double; cdecl; external 'libecode.so' name 'SecToTime';
-function SecToInteger(aSec: longword): integer; cdecl; external 'libecode.so' name 'SecToInteger';
-function MiliSecToTime(aMiliSec: longword): double; cdecl; external 'libecode.so' name 'MiliSecToTime';
-function MiliSecToInteger(aMiliSec: longword): longword; cdecl; external 'libecode.so' name 'MiliSecToInteger';
-function TimeTruncate(Time: longword): longword; cdecl; external 'libecode.so' name 'TimeTruncateInt';
-function TimeTruncate(Time: TDateTime): TDateTime; cdecl; external 'libecode.so' name 'TimeTruncateDT';
-function TimeToInteger(Hour,Minutes,Second,Milisecond: word): longword; cdecl; external 'libecode.so' name 'DecodeTimeToInteger';
-function TimeToInteger(Time: TDateTime): longword; cdecl; external 'libecode.so' name 'TimeToInteger';
-function TimeToInteger: longword; cdecl; external 'libecode.so' name 'NowToInteger';
-function IntegerToTime(czas: longword; no_milisecond: boolean = false): TDateTime; cdecl; external 'libecode.so' name 'IntegerToTime';
+function SecToTime(aSec: longword): double; cdecl; external 'libecode_c.so' name 'fSecToTime';
+function SecToInteger(aSec: longword): longword; cdecl; external 'libecode_c.so' name 'fSecToInteger';
+function MiliSecToTime(aMiliSec: longword): double; cdecl; external 'libecode_c.so' name 'fMiliSecToTime';
+function MiliSecToInteger(aMiliSec: longword): longword; cdecl; external 'libecode_c.so' name 'fMiliSecToInteger';
+function TimeTruncate(Time: longword): longword; cdecl; external 'libecode_c.so' name 'fTimeTruncateInt';
+function TimeTruncate(Time: TDateTime): TDateTime; cdecl; external 'libecode_c.so' name 'fTimeTruncateDT';
+function TimeToInteger(Hour,Minutes,Second,Milisecond: word): longword; cdecl; external 'libecode_c.so' name 'fDecodeTimeToInteger';
+function TimeToInteger(Time: TDateTime): longword; cdecl; external 'libecode_c.so' name 'fTimeToInteger';
+function TimeToInteger: longword; cdecl; external 'libecode_c.so' name 'fNowTimeToInteger';
+function IntegerToTime(czas: longword): TDateTime; cdecl; external 'libecode_c.so' name 'fIntegerToTime';
+function IntegerToTimeNoMs(czas: longword): TDateTime; cdecl; external 'libecode_c.so' name 'fIntegerToTimeNoMs';
+function IntegerToTime(czas: longword; no_milisecond: boolean): TDateTime;
 { ----------------------- KOD 01 ------------------------------- }
 function StringTruncate(s: string; max: integer):string; cdecl; external 'libecode.so' name 'StringTruncate';
 function GetFileSize(filename:string):int64; cdecl; external 'libecode.so' name 'GetFileSize';
@@ -97,12 +135,11 @@ function StringToDate(str:string):TDateTime; cdecl; external 'libecode.so' name 
 procedure BinaryToStrings(var vTab:TStrings; Tab:array of byte); cdecl; external 'libecode.so' name 'BinaryToStrings';
 function GetLineToStr(const S: string; N: Integer; const Delims: Char; const wynik: string = ''): string;
 function GetLineCount(aStr: string; separator: char): integer;
-function LazGetLineToStr(const S: string; N: Integer; const Delims: Char; const wynik: string = ''): string;
-function LazGetLineCount(aStr: string; separator: char): integer;
+function GetLineCount(aStr: string; separator,forcetextseparator: char): integer;
 function GetKeyFromStr(s:string):string; cdecl; external 'libecode.so' name 'GetKeyFromStr';
 function GetIntKeyFromStr(s:string):integer; cdecl; external 'libecode.so' name 'GetIntKeyFromStr';
 procedure SetDir(Directory:string);
-function MyDir(Filename:string='';AddingExeExtension:boolean=false):string;
+function MyDir(Filename: string = ''; AddingExeExtension: boolean = false): string;
 function SetConfDir(Filename: string; Global: boolean = false; OnlyReadOnly: boolean = false): string;
 function SetConfDir(Global: boolean = false; OnlyReadOnly: boolean = false): string;
 function MyConfDir(Filename:string;Global:boolean=false):string;
@@ -119,10 +156,14 @@ function NrDniaToNazwaDnia(nr:integer):string; cdecl; external 'libecode.so' nam
 function GetGenerator(var gen:integer):integer; overload; cdecl; external 'libecode.so' name 'GetGeneratorInt';
 function GetGenerator(var gen:cardinal):cardinal; overload; cdecl; external 'libecode.so' name 'GetGeneratorCardinal';
 //function StringToItemIndex(slist:TStrings;kod:string;wart_domyslna:integer=-1):integer; cdecl; external 'libecode.so' name 'StringToItemIndex';
+procedure StrToListItems(s:string;list:TStrings);
 function StringToItemIndex(slist:TStrings;kod:string;wart_domyslna:integer=-1):integer;
+function StringToItemIndexEx(slist: TStrings; kod: string; aIndeksOd: integer = 0; aIndeksDo: integer = -1; wart_domyslna: integer = -1): integer;
 function ToBoolean(s:string):boolean; overload; cdecl; external 'libecode.so' name 'StrToBoolean';
 function ToBoolean(c:char):boolean; overload; cdecl; external 'libecode.so' name 'CharToBoolean';
 function ToBoolean(i:integer):boolean; overload; cdecl; external 'libecode.so' name 'IntToBoolean';
+function IsCharWord(ch: char): boolean;
+function IsCharHex(ch: char): boolean;
 function IsPeselValid(pesel: string): boolean; cdecl; external 'libecode.so' name 'IsPeselValid';
 function PeselToDate(Pesel:string):TDateTime; cdecl; external 'libecode.so' name 'PeselToDate';
 function PeselToWiek(pesel:string):integer; cdecl; external 'libecode.so' name 'PeselToWiek';
@@ -149,14 +190,15 @@ function StrToHex(str:string):string; cdecl; external 'libecode.so' name 'StrToH
 function NormalizeB(aFormat: string; aWielkoscBajtowa: int64): string; cdecl; external 'libecode.so' name 'NormalizeB';
 function NormalizeFName(s:string):string; cdecl; external 'libecode.so' name 'NormalizeFName';
 { ----------------------- KOD 02 ------------------------------- }
-function IsWhiteChar(c:char):boolean; cdecl; external 'libecode.so' name 'IsWhiteChar';
-function IsDigit(c:char):boolean; cdecl; external 'libecode.so' name 'IsDigit';
-function StrToD(s1:string; var s2:string; znak_dziesietny: char = '.'):double; cdecl; external 'libecode.so' name 'StrToD';
-function StrToDCurr(s1:string; var s2:string; znak_dziesietny: char = '.'):double; cdecl; external 'libecode.so' name 'StrToDCurr';
-function StrToL(s1:string; var s2:string; baza:integer):longint; cdecl; external 'libecode.so' name 'StrToL';
-function AToI(s:string):integer; cdecl; external 'libecode.so' name 'AToI';
-function GToS(d:double;l:integer):string; cdecl; external 'libecode.so' name 'GToS';
-procedure QSort(adr:pointer;size_elementu:longint;adres:pointer;ile,ilosc_elementow:longint); cdecl; external 'libecode.so' name 'QSort';
+function IsWhiteChar(c:char):boolean;
+function IsSpace(c:char):boolean;
+function IsDigit(c:char):boolean;
+function StrToL(s1:string; var s2:string; baza:integer):longint;
+function StrToD(s1:string; var s2:string):double;
+function StrToDCurr(s1:string; var s2:string):double;
+function AToI(s:string):integer;
+function GToS(d:double;l:integer):string;
+procedure QSort(base: pointer; num, size: NativeUInt; compare_func: TComparisonFunc); cdecl; external 'libecode_c.so' name 'fQSort';
 
 {$IFDEF UNIX}
 {$ELSE}
@@ -184,16 +226,22 @@ type
 {$ENDIF}
 
 var
+  GlobalDecimalPoint: char;
   ConfigLocalDirectory: string = '';
   ConfigGlobalDirectory: string = '';
   MyDirectory: string = '';
 
+function fDecimalPoint: char; cdecl; external 'libecode.so' name 'fDecimalPoint';
 function GET_FF: char; cdecl; external 'libecode.so' name '_FF';
-function fGetLineToStr(aStr: Pchar; l: integer; separator,textseparator: char; wynik: pchar; var wartosc: pchar): integer; cdecl; external 'libecode_c';
-function fGetLineCount(aStr: Pchar; separator,textseparator: char): integer; cdecl; external 'libecode_c';
-//function fStringToItemIndex(var slist: pchar; kod: pchar; wart_domyslna: integer): integer; cdecl; external 'libecode_c';
-function lGetLineToStr(const S: string; N: Integer; const Delims,TextSeparator: Char; const wynik: string = ''): string; cdecl; external 'libecode.so' name 'GetLineToStr';
-function lGetLineCount(aStr: string; separator,textseparator: char): integer; cdecl; external 'libecode.so' name 'GetLineCount';
+function fGetLineToStr(aStr: Pchar; l: integer; separator,textseparator: char; wynik: pchar; var wartosc: pchar): integer; cdecl; external 'libecode_c' name 'fGetLineToStr';
+function fGetLineCount(aStr: Pchar; separator,textseparator: char): integer; cdecl; external 'libecode_c' name 'fGetLineCount';
+function fIsSpace(c: char): integer; cdecl; external 'libecode_c' name 'fIsSpace';
+function fIsDigit(c: char): integer; cdecl; external 'libecode_c' name 'fIsDigit';
+function fStrToL(aStr: Pchar; var reszta: Pchar; baza: integer): integer; cdecl; external 'libecode_c' name 'fStrToL';
+function fStrToD(aStr: Pchar; var reszta: Pchar): double; cdecl; external 'libecode_c' name 'fStrToD';
+function fAToI(aStr: pchar): integer; cdecl; external 'libecode_c' name 'fAToI';
+function fGToS(d: double; l: integer; var wynik: pchar): integer; cdecl; external 'libecode_c' name 'fGToS';
+//function fStringToItemIndex(var slist: pchar; kod: pchar; wart_domyslna: integer): integer; cdecl; external 'libecode_c' name 'fStringToItemIndex';
 
 function GetLocaleDefault(res_out: integer): string;
 var
@@ -209,6 +257,11 @@ type
 
 var
   ppp: pchar;
+
+function IntegerToTime(czas: longword; no_milisecond: boolean): TDateTime;
+begin
+  if no_milisecond then result:=IntegerToTimeNoMs(czas) else result:=IntegerToTime(czas);
+end;
 
 //Funkcja zwraca n-ty (l) ciąg stringu (s), o wskazanym separatorze.
 //Ewentualnie zwraca wynik, jeśli string będzie pusty, gdy oczywiście się go wypełni!
@@ -228,15 +281,10 @@ begin
   result:=fGetLineCount(pchar(aStr),separator,textseparator);
 end;
 
-function LazGetLineToStr(const S: string; N: Integer; const Delims: Char;
-  const wynik: string): string;
+function GetLineCount(aStr: string; separator, forcetextseparator: char
+  ): integer;
 begin
-  result:=lGetLineToStr(S,N,Delims,textseparator,wynik);
-end;
-
-function LazGetLineCount(aStr: string; separator: char): integer;
-begin
-  result:=lGetLineCount(aStr,separator,textseparator);
+  result:=fGetLineCount(pchar(aStr),separator,forcetextseparator);
 end;
 
 procedure SetDir(Directory:string);
@@ -330,6 +378,27 @@ begin
   result:=MyConfDir('',Global);
 end;
 
+procedure StrToListItems(s: string; list: TStrings);
+var
+  i: integer;
+  pom: string;
+begin
+  list.Clear;
+  pom:='';
+  for i:=1 to length(s) do
+  begin
+    if s[i]=#10 then
+    begin
+      list.Add(pom);
+      pom:='';
+      continue;
+    end;
+    if s[i]=#13 then continue;
+    pom:=pom+s[i];
+  end;
+  if pom<>'' then list.Add(pom);
+end;
+
 function StringToItemIndex(slist: TStrings; kod: string; wart_domyslna: integer
   ): integer;
 var
@@ -342,6 +411,85 @@ begin
     break;
   end;
   result:=a;
+end;
+
+function StringToItemIndexEx(slist: TStrings; kod: string; aIndeksOd: integer;
+  aIndeksDo: integer; wart_domyslna: integer): integer;
+var
+  i,a,max: integer;
+begin
+   a:=wart_domyslna;
+   if aIndeksDo=-1 then max:=slist.Count-1 else max:=aIndeksDo;
+   for i:=aIndeksOd to max do if slist[i]=kod then
+   begin
+     a:=i;
+     break;
+   end;
+   result:=a;
+end;
+
+function IsCharWord(ch: char): boolean;
+begin
+  Result:= ch in ['a'..'z', 'A'..'Z', '_', '0'..'9'];
+end;
+
+function IsCharHex(ch: char): boolean;
+begin
+  Result:= ch in ['0'..'9', 'a'..'f', 'A'..'F'];
+end;
+
+function IsWhiteChar(c: char): boolean;
+begin
+  result:=fIsSpace(c)=1;
+end;
+
+function IsSpace(c: char): boolean;
+begin
+  result:=fIsSpace(c)=1;
+end;
+
+function IsDigit(c: char): boolean;
+begin
+  result:=fIsDigit(c)=1;
+end;
+
+function StrToL(s1: string; var s2: string; baza: integer): longint;
+var
+  pom: pchar = nil;
+begin
+  result:=fStrToL(pchar(s1),&pom,baza);
+  s2:=StrPas(pom);
+end;
+
+function StrToD(s1: string; var s2: string): double;
+var
+  s: string;
+  pom: pchar = nil;
+begin
+  if GlobalDecimalPoint='.' then s:=StringReplace(s1,',','.',[rfReplaceAll]) else s:=StringReplace(s1,'.',',',[rfReplaceAll]);
+  result:=fStrToD(pchar(s),&pom);
+  s2:=StrPas(pom);
+end;
+
+function StrToDCurr(s1: string; var s2: string): double;
+var
+  pom: pchar = nil;
+begin
+  result:=fStrToD(pchar(s1),&pom);
+  s2:=StrPas(pom);
+end;
+
+function AToI(s: string): integer;
+begin
+  result:=fAToI(pchar(s));
+end;
+
+function GToS(d: double; l: integer): string;
+var
+  len: SizeInt;
+begin
+  len:=fGToS(d,l,&ppp);
+  SetString(result,ppp,len);
 end;
 
 {$IFDEF UNIX}
@@ -463,6 +611,7 @@ end;
 
 initialization
   ppp:=nil;
+  GlobalDecimalPoint:=fDecimalPoint;
   _FF:=GET_FF;
 finalization
   if ppp<>nil then StrDispose(ppp);
